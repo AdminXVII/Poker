dd146dce42e4f0ae86a2c1c79f445e559c3c5239
YouniceBen <45801372+YouniceBen@users.noreply.github.com>
2018-12-11 15:52:58 -0500

    Add files via upload

--- Player.java
+++ Player.java
@@ -5,9 +5,9 @@ public class Player {
 	String name;
 	String[] hand = new String[2];
 	
-	public Player(String name,int chips){
+	public Player(String name){
 		
-		this.chips = chips;
+		this.chips = 1000;
 		this.name = name;
 		
 	}
--- Table.java
+++ Table.java
@@ -5,7 +5,16 @@ public class Table {
 	
 	public static void main(String[] args) {
 			
-		System.out.println(Dealer.deck.length);
+		Player younes = new Player("Younes");
+		Player andrei = new Player("Andrei");
+		Player anthony = new Player("Anthony");
+		
+		Player[] players = {younes,andrei,anthony};
+		
+		Dealer.deal(players);
+		System.out.println(younes.hand[0] + " " + younes.hand[1] + " " +
+				andrei.hand[0] + " " + andrei.hand[1] + " " +
+				anthony.hand[0] + " " + anthony.hand[1]);
 		
 		}
 	
